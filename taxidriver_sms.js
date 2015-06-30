947ed4984a9dc3964c61def72e8b2e7054a8e049
Brian J. Burroughs <bburroughs@orionsmind.com>
2015-06-30 14:11:52 -0500

    retrieve request:

--- taxidriver_sms.js
+++ taxidriver_sms.js
@@ -1,24 +1,98 @@
-var accountSid = 'AC0cabfe79a2b5b8273320781fca4c09ed';
-var authToken = "985d9014c96813f6110cde04b83bb8bb";
+var request = require('request');
 var twilio = require('twilio');
 var mongoose = require('mongoose');
 var express = require('express');
 var app = express();
+
+var accountSid = 'AC0cabfe79a2b5b8273320781fca4c09ed';
+var authToken = "985d9014c96813f6110cde04b83bb8bb";
 var client = new twilio.RestClient(accountSid, authToken);
+
+
 
 app.set('port', (process.env.PORT || 5000));
 
+//'mongodb://taxidriver:taxidriver@host:port/database?options...'
+//mongodb://taxidriver:taxidriver@ds041992.mongolab.com:41992
+mongoose.connect('mongodb://taxidriver:taxidriver@ds041992.mongolab.com:41992/taxidriver');
+var locSchema = mongoose.Schema({
+	Address: String,
+	Amount: Number,
+	Description: String,
+	Date: Date,
+	loc: []
+});
+var nearviolations = mongoose.model('violations', locSchema);
+
+var geocodeer = {
+	limit: 2500, //day
+	url: "https://maps.googleapis.com/maps/api/geocode/json?address={{ADDRESS}}&key={{TOKEN}}",
+	token: "AIzaSyBqK4f8zbMrK4K5cxWb8_10Zkbk7LHMrKE",
+	requesturl: function(url, address, token) {
+		return url.replace("{{ADDRESS}}", address).replace("{{TOKEN}}", token);
+	},
+	response: function(body) {
+		if (body.results != undefined && body.results[0] != undefined && body.results[0].geometry != undefined && body.results[0].geometry.location != undefined) {
+			var loc = body.results[0].geometry.location;
+			var addr = changeCase.upper(body.results[0].formatted_address);
+			var cmpnts = body.results[0].address_components;
+			var shortaddr = cmpnts[0]["short_name"] + " " + changeCase.upper(cmpnts[1]["short_name"]) + "," + cmpnts[2]["short_name"];
+
+			return {
+				loc: loc,
+				fulladdress: addr
+			};
+		}
+
+		return null;
+	}
+};
+
 app.get('/', function(request, response) {
 
-	client.messages.create({
-		to: '+17739809873',
-		from: '+17736090911',
-		body: 'Hello World'
-	}, function(error, message) {
-		if (error) {
-			console.log(error.message);
+	//recieve address
+	var address = request.body;
+
+	//geoloc address
+	
+	var gourl = geocodeer.requesturl(geocodeer.url, address, geocodeer.token);
+
+	console.log(address);
+	response.send("Computing...");
+	request(gourl, function(err, res, body) {
+		if (err) {
+			JSON.stringify(err);
+			return done(err);
 		}
+		if (res.statusCode != 200) return done(res.statusCode);
+		done();
+		var location = geocodeer.response(body);
+
+		/*
+		if (!location)
+			response.send("Submitted Address Failed");
+		//return violations
+		var rs = nearviolations.find({
+			loc: {
+				'$near': [location.lng, location.lat]
+			}
+		}).limit(5);
+		//return no violations
+
+		client.messages.create({
+			to: '+17739809873',
+			from: '+17736090911',
+			body: JSON.stringify(rs)
+		}, function(error, message) {
+			if (error) {
+				response.send(error.message);
+			}
+		});
+		*/
+
 	});
+
+	response.send("made it this far");
 });
 
 app.listen(app.get('port'), function() {
