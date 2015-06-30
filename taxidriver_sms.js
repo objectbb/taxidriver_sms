9d00b8c9d50257069dece8d8a3045b2380d26e15
Brian J. Burroughs <bburroughs@orionsmind.com>
2015-06-29 21:31:31 -0500

    add express handle web request

--- taxidriver_sms.js
+++ taxidriver_sms.js
@@ -2,14 +2,25 @@ var accountSid = 'AC0cabfe79a2b5b8273320781fca4c09ed';
 var authToken = "985d9014c96813f6110cde04b83bb8bb";
 var twilio = require('twilio');
 var mongoose = require('mongoose');
+var express = require('express');
+var app = express();
 var client = new twilio.RestClient(accountSid, authToken);
- 
-client.messages.create({
-    to:'+17739809873',
-    from:'+17736090911',
-    body:'Hello World'
-}, function(error, message) {
-    if (error) {
-        console.log(error.message);
-    }
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(request, response) {
+
+	client.messages.create({
+		to: '+17739809873',
+		from: '+17736090911',
+		body: 'Hello World'
+	}, function(error, message) {
+		if (error) {
+			console.log(error.message);
+		}
+	});
 });
+
+app.listen(app.get('port'), function() {
+	console.log("Node app is running on port:" + app.get('port'))
+})
